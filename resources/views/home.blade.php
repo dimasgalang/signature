0746e26e22605e04a7c9db6361574c33091ca84a
Dimas Galang <dimasgalang@student.com>
2025-03-14 15:17:08 +0700

    change homepage

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -124,12 +124,6 @@
                             <img class="img-fluid px-3 px-sm-4 mt-3 mb-4" width="100%"
                                 src="{{asset('img/e-signature.jpg')}}" alt="...">
                         </div>
-                        <p>Add some quality, svg illustrations to your project courtesy of <a
-                                target="_blank" rel="nofollow" href="https://undraw.co/">unDraw</a>, a
-                            constantly updated collection of beautiful svg images that you can use
-                            completely free and without attribution!</p>
-                        <a target="_blank" rel="nofollow" href="https://undraw.co/">Browse Illustrations on
-                            unDraw &rarr;</a>
                     </div>
                 </div>
             </div>
