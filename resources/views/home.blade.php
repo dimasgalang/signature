f60c217b87d385d4009a0b33d1905cde58b33a70
Dimas Galang <dimasgalang@student.com>
2025-03-19 08:52:24 +0700

    update ilustration

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -119,12 +119,12 @@
                 </div>
                 <div class="card shadow mb-4">
                     <div class="card-header py-3">
-                        <h6 class="m-0 font-weight-bold text-primary">Illustrations</h6>
+                        <h6 class="m-0 font-weight-bold text-primary">Chutex E-Signature Flow</h6>
                     </div>
                     <div class="card-body">
                         <div class="text-center">
                             <img class="img-fluid px-3 px-sm-4 mt-3 mb-4" width="100%"
-                                src="{{asset('img/e-signature.jpg')}}" alt="...">
+                                src="{{asset('storage/images/ilustration.png')}}" alt="...">
                         </div>
                     </div>
                 </div>
